ad3c0c17be4d40550e85590389e8a35adba3a9c9
Thomas, Alwin <athomas423@gatech.edu>
2022-05-24 03:15:50 -0400

    reverted code to original before logging errors

---
+++ src/main/java/com/springframework/msscbrewery/services/CustomerServiceImpl.java
@@ -0,0 +1,17 @@
+package com.springframework.msscbrewery.services;
+
+import com.springframework.msscbrewery.web.model.CustomerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public CustomerDto getCustomerById(UUID customerId) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .name("Joe Buck")
+                .build();
+    }
+}
